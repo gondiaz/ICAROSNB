9868ff6ab747abff931a33fa90fdd02a7e2eeced
gondiaz <gonzalodiazlopez10@gmail.com>
2019-07-30 11:29:17 +0200

    Update

--- GDL/irene_validation/pmaps_summary.py
+++ GDL/irene_validation/pmaps_summary.py
@@ -10,18 +10,21 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i" , "--indir"       , type=str , help="pmapdir")
+parser.add_argument("-i" , "--indir"       , type=str, help="pmapdir")
 parser.add_argument("-r" , "--nfiles_range", type=int,  help="file range", nargs=2)
-parser.add_argument("-o"  , "--out"        , type=str , help="outdir")
+parser.add_argument("-o"  , "--out"        , type=str, help="outdir")
+parser.add_argument("-sl"  , "--slidecut"     , type=int, help="Cut in # of files for slide data")
+parser.add_argument("-si"  , "--sicut"        , type=int, help="Cut in # of files fos sipm data")
 
 args = parser.parse_args(sys.argv[1:])
 
 pmapdir     = args.indir
 pmap_range  = args.nfiles_range
 outfile     = args.out
+sldcut      = args.slidecut
+sicut       = args.sicut
 
-
-def create_and_fill_summary_file(pmapdir, outfile, pmap_range, sldcut = 100):
+def create_and_fill_summary_file(pmapdir, outfile, pmap_range, sldcut, sicut):
 
     #### CREATE FILE ####
     h5file = tb.open_file(outfile, mode="w", title="Pmap Summary")
@@ -38,6 +41,10 @@ def create_and_fill_summary_file(pmapdir, outfile, pmap_range, sldcut = 100):
     Sldgroup = h5file.create_group("/", 'Slide', 'Slide info')
     Sldtable = h5file.create_table(Sldgroup, 'Sld', slide_info, title="Slide info table")
 
+
+    SIPMgroup = h5file.create_group("/", "SIPM", "Charge by Sipm")
+
+
     #### FILL FILE ####
     FILES = glob.glob(pmapdir + "/*")
     FILES.sort()
@@ -45,6 +52,7 @@ def create_and_fill_summary_file(pmapdir, outfile, pmap_range, sldcut = 100):
     FILES = FILES[start : stop]
 
     processed = 1
+    arr = np.ndarray(shape=(0, 2))
     for pmap in FILES:
 
         if pmap.split('_')[-3]=="v0.9.9":
@@ -62,29 +70,34 @@ def create_and_fill_summary_file(pmapdir, outfile, pmap_range, sldcut = 100):
         Nev.append()
 
         #### Fill S1s ####
-        loop_in_events_PMT(S1table.row, S1pmap, S1PMTpmap, EVENTS)
+        loop_in_events_S1(S1table.row, S1pmap, S1PMTpmap, EVENTS)
 
-        ##### Fill S2s PMT data ####
-        loop_in_events_PMT (S2table.row,              S2pmap, S2PMTpmap, EVENTS)
+        #### Fill S1s ####
+        if processed>sldcut: Sld = None
+        else:                Sld = Sldtable.row
+        loop_in_events_S2(S2table.row, Sld, S2pmap, S2PMTpmap, S2SIpmap, EVENTS, file_number)
 
-        if processed>=sldcut:
-            Sld = False
-        else:
-            Sld = Sldtable.row
-        loop_in_events_S2SI(S2table.row,         Sld, S2pmap, S2SIpmap, EVENTS)
 
-        processed =+ 1
+        if processed<=sicut or sicut>=len(FILES):
+            sipm_q = S2SIpmap[S2SIpmap["ene"]>0]
+            arr = np.concatenate( (arr, sipm_q[["nsipm", "ene"]].values))
+
+            if processed==sicut or processed==len(FILES):
+                sipmarray = h5file.create_array(SIPMgroup, 'sipm_charge', arr, "SIPM charges")
+                sipmarray.flush()
+
+        processed += 1
 
     #### Save data to disk and close ####
-    Nevtable.flush()
-    S1table .flush()
-    S2table .flush()
-    Sldtable.flush()
+    Nevtable .flush()
+    S1table  .flush()
+    S2table  .flush()
+    Sldtable .flush()
 
     h5file.close()
 
 
-def loop_in_events_PMT(S, Spmap, SPMTpmap, EVENTS):
+def loop_in_events_S1(S, Spmap, SPMTpmap, EVENTS):
     for ev in EVENTS:
         Sgn  = Spmap   [Spmap   ["event"]==ev]
         SPMT = SPMTpmap[SPMTpmap["event"]==ev]
@@ -104,7 +117,7 @@ def loop_in_events_PMT(S, Spmap, SPMTpmap, EVENTS):
             PMTS  = np.unique(SPMT["npmt"])
             esum = []
             for pmt in PMTS:
-                enes = SPMT[SPMT["npmt"]==pmt]["ene"].values
+                enes = SPMT[(SPMT["peak"]==pk)&(SPMT["npmt"]==pmt)]["ene"].values
                 esum.append( np.sum( enes[enes>0]) )
 
             S["most_active_PMT"] = PMTS[np.argmax(esum)]
@@ -113,28 +126,45 @@ def loop_in_events_PMT(S, Spmap, SPMTpmap, EVENTS):
             S.append()
 
 
-def loop_in_events_S2SI(S, Sld, S2pmap, S2Sipmap, EVENTS):
+def loop_in_events_S2(S, Sld, Spmap, SPMTpmap, SSIpmap, EVENTS, file_number):
     for ev in EVENTS:
-        S2   = S2pmap  [S2pmap  ["event"]==ev]
-        S2SI = S2Sipmap[S2Sipmap["event"]==ev]
+        Sgn  = Spmap   [Spmap   ["event"]==ev]
+        SPMT = SPMTpmap[SPMTpmap["event"]==ev]
+        SSI  = SSIpmap [SSIpmap ["event"]==ev]
 
-        #loop in peaks
-        PEAKS = np.unique(S2["peak"])
+        PEAKS = np.unique(Sgn["peak"])
         for pk in PEAKS:
-            S2_pk   = S2  [S2["peak"]==pk]
-            S2SI_pk = S2SI[ S2SI["peak"] == pk ]
+            #Total
+            times = Sgn[Sgn["peak"]==pk]["time"].values
+            enes  = Sgn[Sgn["peak"]==pk]["ene"] .values
 
-            S["charge"]        = np.sum( S2SI_pk["ene"] )
-            S["touched_sipms"] = len(np.unique(S2SI_pk["nsipm"]))
+            S["event"]  = ev
+            S["file" ]  = file_number
+            S["time"]   = times[np.argmax(enes)]/10**3
+            S["height"] = np.max(enes)
+            S["energy"] = np.sum(enes)
+            S["width"]  = times[-1]-times[0]
+            S["charge"]        = np.sum( SSI[ SSI["peak"] == pk ]["ene"] )
+            S["touched_sipms"] = len(np.unique(SSI[ SSI["peak"] == pk ]["nsipm"]))
+
+            PMTS  = np.unique(SPMT["npmt"])
+            esum = []
+            for pmt in PMTS:
+                enes = SPMT[(SPMT["peak"]==pk)&(SPMT["npmt"]==pmt)]["ene"].values
+                esum.append( np.sum( enes[enes>0]) )
+
+            S["most_active_PMT"] = PMTS[np.argmax(esum)]
+            S["energy_fraction_most_active_PMT"] = np.max(esum)/np.sum(esum)*100
 
             S.append()
 
+            #slides
             if Sld:
-                #slides
-                nslides = len(S2_pk)
-                sipms   = np.unique( S2SI_pk["nsipm"].values )
+                nslides = len( Sgn[Sgn["peak"]==pk] )
+                sipms   = np.unique( SSI[ SSI["peak"] == pk ]["nsipm"] )
+                if len(sipms)==0: continue
 
-                splitted = np.array( np.split(S2SI_pk["ene"].values, len(sipms)) ).T
+                splitted = np.array( np.split(SSI[ SSI["peak"] == pk ]["ene"].values, len(sipms)) ).T
                 charge_per_slide = np.sum(splitted, axis=0)
 
                 for slide in splitted:
@@ -155,15 +185,16 @@ class S1Signal(tb.IsDescription):
 
 class S2Signal(tb.IsDescription):
     event    = tb.Int64Col  (pos=0)
-    time     = tb.Float64Col(pos=1)
-    height   = tb.Float64Col(pos=2)
-    energy   = tb.Float64Col(pos=3)
-    width    = tb.Float64Col(pos=4)
-    most_active_PMT = tb.Int64Col(pos=5)
-    energy_fraction_most_active_PMT = tb.Float64Col(pos=6)
+    file     = tb.Int64Col  (pos=1)
+    time     = tb.Float64Col(pos=2)
+    height   = tb.Float64Col(pos=3)
+    energy   = tb.Float64Col(pos=4)
+    width    = tb.Float64Col(pos=5)
+    most_active_PMT = tb.Int64Col(pos=6)
+    energy_fraction_most_active_PMT = tb.Float64Col(pos=7)
 
-    charge        = tb.Float64Col(pos=7)
-    touched_sipms = tb.Int64Col  (pos=8)
+    charge        = tb.Float64Col(pos=8)
+    touched_sipms = tb.Int64Col  (pos=9)
 
 class n_events(tb.IsDescription):
     file_number = tb.Int64Col(pos=0)
@@ -176,4 +207,4 @@ class slide_info(tb.IsDescription):
 
 
 if __name__ == "__main__":
-    create_and_fill_summary_file(pmapdir, outfile, pmap_range, sldcut = 100)
+    create_and_fill_summary_file(pmapdir, outfile, pmap_range, sldcut, sicut)
